b9d93fbd76c694b0cee8c19cce1887abc40f1763
taeminHan <taemin9705@gmail.com>
2024-01-12 11:24:53 +0900

    Time: 27 ms (5.96%), Space: 44.6 MB (5.81%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,24 +1,26 @@
 class Solution {
 
-    private String[] VOWELS = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"};   
+    private static final String[] VOWELS = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"};
 
     public boolean halvesAreAlike(String s) {
         String[] splitStr = s.split("");
 
         int count = s.length();
+        int mid = count / 2;
 
-        int frontCount = half(splitStr, 0, count/2);
-        int rearCount = half(splitStr, count/2, count);
+        int frontCount = countVowels(splitStr, 0, mid);
+        int rearCount = countVowels(splitStr, mid, count);
 
-        return frontCount==rearCount;
+        return frontCount == rearCount;
     }
 
-    private int half(String[] str, int start, int end){
+    private int countVowels(String[] str, int start, int end) {
         int cnt = 0;
-        for(int i=start;i<end;i++){
-            for(String vowel:VOWELS){
-                if(str[i].equals(vowel)){
+        for (int i = start; i < end; i++) {
+            for (String vowel : VOWELS) {
+                if (str[i].equals(vowel)) {
                     cnt++;
+                    break;  // Break once a vowel is found in the inner loop
                 }
             }
         }
